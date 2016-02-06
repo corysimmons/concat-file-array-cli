4b3e42320be02f770e2abce8c731de9560533904
Cory Simmons <cory@launchboxhq.com>
2016-02-06 04:09:26 -0500

    gogogo

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # concat-file-array-cli
+
 CLI to concat an array of files.
+
+### Installation
+`npm install concat-file-array-cli`
+
+### Usage
+`concat-files foo.txt bar.txt -o baz.txt`
---
+++ cli.js
@@ -0,0 +1,18 @@
+var meow = require('meow');
+var concat = require('concat');
+
+var cli = meow('Usage: concat-files foo.js bar.js baz.js -o bundle.js', {
+  alias: {
+    h: 'help',
+    o: 'output'
+  }
+});
+
+if (cli.input && cli.flags.output) {
+  concat(cli.input, cli.flags.output, function (err) {
+    if (err) throw err;
+    console.log('\nFiles have been bundled.\n');
+  });
+} else {
+  console.log('\nError: Need an input and output file. concat-files -h for usage.\n');
+}
